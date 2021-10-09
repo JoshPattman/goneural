003b2af71ac333f21e19d8385256c3e43761a657
Josh Pattman <Josh.pattman@gmail.com>
2021-10-09 20:24:04 +0100

    removed bad tests

--- goneural_test.go
+++ goneural_test.go
@@ -1,25 +1,20 @@
 package goneural
 
-import (
-	"fmt"
-	"testing"
-)
+import "testing"
 
-func TestFP(t *testing.T) {
-	n := createNetwork()
-	fmt.Println(n.Calculate([]float64{0, 1, -2}))
-	fmt.Println(n.ToJSON())
+func TestFeedForward(t *testing.T) {
+	l, err := NewFeedForwardLayer(5, 3, "relu")
+	checkErr(err, t)
+	vals, err := l.Calculate([]float64{})
+	checkErr(err, t)
+	if len(vals) != 3{
+		t.Error("Layer did not give expected number of outputs")
+	}
+
 }
 
-
-func createNetwork()*Network{
-	l1 := NewFeedForwardLayer(3, 5, "linear")
-	l2 := NewFeedForwardLayerAfter(l1, 5, "leaky_relu")
-	l3 := NewFeedForwardLayerAfter(l2, 1, "sigmoid")
-	n := NewNetwork([]Layer{
-		l1,
-		l2,
-		l3,
-	})
-	return n
+func checkErr(err error, t *testing.T){
+	if err != nil{
+		t.Error(err.Error())
+	}
 }
